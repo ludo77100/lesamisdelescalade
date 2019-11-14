c870fe252fb8efb9cb5369cd30561db83346708b
ludo77100 <ludovic.s@protonmail.com>
2019-11-14 10:51:54 +0100

    Ajout entities et la connexion à la base de donnée ladeDB

---
+++ src/main/java/com/ludo/entities/Commentaire.java
@@ -0,0 +1,7 @@
+package com.ludo.entities;
+
+import java.io.Serializable;
+
+public class Commentaire implements Serializable {
+
+}
---
+++ src/main/java/com/ludo/entities/Utilisateur.java
@@ -0,0 +1,7 @@
+package com.ludo.entities;
+
+import java.io.Serializable;
+
+public class Utilisateur implements Serializable {
+
+}
