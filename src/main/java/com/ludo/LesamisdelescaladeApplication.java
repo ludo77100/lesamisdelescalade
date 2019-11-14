26dedf581ad5ae27b516c925e979067d555dd778
ludo77100 <ludovic.s@protonmail.com>
2019-11-14 08:37:28 +0100

    Premier commit

---
+++ src/main/java/com/ludo/LesamisdelescaladeApplication.java
@@ -0,0 +1,13 @@
+package com.ludo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LesamisdelescaladeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LesamisdelescaladeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ludo/LesamisdelescaladeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ludo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LesamisdelescaladeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
