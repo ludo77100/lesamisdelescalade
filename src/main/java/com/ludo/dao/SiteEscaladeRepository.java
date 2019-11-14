e4e8782eb38de07bfd0f1d2da6d2cfe8d43d2086
ludo77100 <ludovic.s@protonmail.com>
2019-11-14 15:10:31 +0100

    Ajout d'un layout

---
+++ src/main/java/com/ludo/dao/SiteEscaladeRepository.java
@@ -0,0 +1,8 @@
+package com.ludo.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.ludo.entities.SiteEscalade;
+
+public interface SiteEscaladeRepository extends JpaRepository<SiteEscalade, Long>{
+
+}
---
+++ src/main/java/com/ludo/web/SiteEscaladeController.java
@@ -0,0 +1,19 @@
+package com.ludo.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ludo.entities.SiteEscalade;
+
+@Controller
+public class SiteEscaladeController {
+@Autowired
+	private SiteEscalade siteEscalade ;
+	@RequestMapping("siteEscalade")
+	public String index() {
+		return "siteEscalade" ;
+	}
+	
+	
+}
