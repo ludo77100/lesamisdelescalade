e50325c3769e1b76b5608cf3de5ac6f1886d971f
ludo77100 <ludovic.s@protonmail.com>
2019-11-15 22:35:42 +0100

    ajout de deux box dans l'index, une permettant d'afficher un edito qui s'appel le mot du président pour le moment, et une autre pour afficher des actualités

--- src/main/java/com/ludo/web/SiteEscaladeController.java
+++ src/main/java/com/ludo/web/SiteEscaladeController.java
@@ -7,24 +7,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
 import com.ludo.dao.SiteEscaladeRepository;
 import com.ludo.entities.SiteEscalade;
 
-
 @Controller
 public class SiteEscaladeController {
-@Autowired
-	private SiteEscaladeRepository siteEscaladeRepository ;
+	@Autowired
+	private SiteEscaladeRepository siteEscaladeRepository;
+
 	@GetMapping("/listeSiteEscalade")
 	public String listeSiteEscalade(Model model) {
 		List<SiteEscalade> siteEscalade = siteEscaladeRepository.findAll();
 		model.addAttribute("listeSiteEscalade", siteEscalade);
-		return "ListeSiteEscalade" ;
+		return "ListeSiteEscalade";
 	}
+
 	@GetMapping("/index")
-public String index() {
-	return "index";
-	
-}
+	public String index() {
+		return "index";
+
+	}
 }
