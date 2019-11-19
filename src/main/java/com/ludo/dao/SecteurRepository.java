ea70d10b133fe1bc0682bdcd126016269e2fc3c4
ludo77100 <ludovic.s@protonmail.com>
2019-11-19 12:47:46 +0100

    modélisation du secteur

--- src/main/java/com/ludo/dao/SecteurRepository.java
+++ src/main/java/com/ludo/dao/SecteurRepository.java
@@ -1,9 +1,11 @@
 package com.ludo.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.ludo.entities.Secteur;
 
+@Repository
 public interface SecteurRepository extends JpaRepository<Secteur, Long>{
 
 }
