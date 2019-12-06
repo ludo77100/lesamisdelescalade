d0e6414910fb03dfc4ffeddd56546662c76426f3
ludo77100 <ludovic.s@protonmail.com>
2019-12-06 04:16:53 +0100

    affichage longueur marche

--- src/main/java/com/ludo/web/LongueurController.java
+++ src/main/java/com/ludo/web/LongueurController.java
@@ -17,6 +17,7 @@ import com.ludo.dao.VoieRepository;
 import com.ludo.entities.Secteur;
 import com.ludo.entities.Spot;
 import com.ludo.entities.Voie;
+import com.ludo.forms.LongueurForms;
 import com.ludo.forms.VoieForm;
 import com.ludo.metier.LongueurService;
 import com.ludo.metier.SecteurService;
@@ -26,38 +27,39 @@ import com.ludo.metier.VoieService;
 @Controller
 public class LongueurController {
 	@Autowired
-	private SpotRepository spotRepository ;
+	private SpotRepository spotRepository;
 	@Autowired
-	private SecteurRepository secteurRepository ;
+	private SecteurRepository secteurRepository;
 	@Autowired
-	private VoieRepository voieRepository ;
+	private VoieRepository voieRepository;
 	@Autowired
-	private LongueurRepository longueurRepository ;
+	private LongueurRepository longueurRepository;
 	@Autowired
-	private SecteurService secteurService ;
+	private SecteurService secteurService;
 	@Autowired
-	private SpotService spotService ;
+	private SpotService spotService;
 	@Autowired
-	private VoieService voieService ;
+	private VoieService voieService;
 	@Autowired
-	private LongueurService longueurService ;
-	
+	private LongueurService longueurService;
+
 	@GetMapping("/spot/{spotId}/secteur/{secteurId}/voie/{voieId}/ajouterLongueur")
-	public String formLongueur(Model model, @PathVariable("spotId")Long spotId, @PathVariable("secteurId")Long secteurId,@PathVariable("voieId")Long voieId) {
+	public String formLongueur(Model model, @PathVariable("spotId") Long spotId,
+			@PathVariable("secteurId") Long secteurId, @PathVariable("voieId") Long voieId) {
 		Spot spot = spotRepository.findById(spotId).get();
 		Secteur secteur = secteurRepository.findById(secteurId).get();
 		Voie voie = voieRepository.findById(voieId).get();
-		return "formLongueur" ;
-}
-	
-	/*
-	 * @PostMapping("/spot/{spotId}/secteur/{secteurId}/ajouterVoie/save") public
-	 * String saveVoie(Model model, @ModelAttribute("voieForm")VoieForm
-	 * voieForm, @PathVariable("spotId")Long spotId, @PathVariable("secteurId")Long
-	 * secteurId, BindingResult result, RedirectAttributes redirectAttributes) {
-	 * 
-	 * voieService.saveVoie(secteurId, voieForm, result);
-	 * 
-	 * return "redirect:/spot/" + spotId + "/secteur/" + secteurId ; }
-	 */
+		return "formLongueur";
+	}
+
+	@PostMapping("/spot/{spotId}/secteur/{secteurId}/voie/{voieId}/ajouterLongueur/save")
+	public String saveLongueur(Model model, @ModelAttribute("longueurForm") LongueurForms longueurForms,
+			@PathVariable("spotId") Long spotId, @PathVariable("secteurId") Long secteurId,
+			@PathVariable("voieId") Long voieId, BindingResult result, RedirectAttributes redirectAttributes) {
+
+		longueurService.saveLongueur(voieId, longueurForms, result);
+
+		return "redirect:/spot/" + spotId + "/secteur/" + secteurId + "/voie/" + voieId;
+	}
+
 }
