8f02d9372b3a9ca9f394d5a2cdb9f7d2c5a20bc2
ludo77100 <ludovic.s@protonmail.com>
2019-12-20 16:03:28 +0100

    ajout de bloquage d'accès au formulaire d'authentification si déjà authentifé

--- src/main/java/com/ludo/security/SecurityController.java
+++ src/main/java/com/ludo/security/SecurityController.java
@@ -1,15 +1,30 @@
 package com.ludo.security;
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class SecurityController {
 
+	/*
+	 * Controller qui permet d'afficher le formulaire de connexion
+	 * l'accès à ce formulaire est blqué si l'utilisateur est déjà authentifier
+	 */
 	@GetMapping("/connexionUtilisateur")
-	public String connexionUtilisateur() {
-		return "formConnexion";
+	public ModelAndView connexionUtilisateur(Model model) {
+		
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		
+		if (!(auth instanceof AnonymousAuthenticationToken)) {
+			return new ModelAndView("redirect:/index");
+		}
+		return new ModelAndView("formConnexion");
 	}
 	
 	@RequestMapping(value = "/403")
