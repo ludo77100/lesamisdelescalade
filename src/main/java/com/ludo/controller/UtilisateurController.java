49882cafa2b423818515567313ca8d0a326ea04a
ludo77100 <ludovic.s@protonmail.com>
2020-02-13 14:47:28 +0100

    divers correction

--- src/main/java/com/ludo/controller/UtilisateurController.java
+++ src/main/java/com/ludo/controller/UtilisateurController.java
@@ -11,10 +11,9 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
+
 import org.springframework.web.bind.annotation.PostMapping;
 
-import com.ludo.dao.UtilisateurRepository;
 import com.ludo.dto.UtilisateurDto;
 import com.ludo.entities.Utilisateur;
 import com.ludo.service.UtilisateurService;
